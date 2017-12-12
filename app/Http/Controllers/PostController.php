309f2dba384efb5cf09e0a1645480d92abe8f80f
Zavin Hussein <zavinhussein@gmail.com>
2017-12-12 22:55:01 +0200

    First Commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('welcome')->with('posts', $posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->save();
+
+        $posts = Post::all();
+
+        return view('welcome')->with('posts', $posts);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        Log::info('Showing user profile for user: '.$id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        Log::info('editing user profile for user: '.$id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+//        $post = Post::find($id);
+//        $post->delete();
+//
+//        return redirect()->back();
+        Log::debug('deleting user profile for user: '.$id);
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-primary mt-3 header text-center ">TO Do List</h1>
+    <form method="POST" action="/">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Add todo" name="title">
+        </div>
+    </form>
+    <ul>
+        @if(count($posts) > 0)
+            @foreach($posts as $post)
+                <div class="d-flex flex-row m-2">
+                    <input class="form-check-input" type="checkbox" value="">
+                    <h4 class="d-inline">{{$post->title}}</h4>
+                    {{--<a class="btn btn-danger ml-auto" href="/" method="DELETE">Delete</a>--}}
+                    {!! Form::open(['route' => ['PostController@destroy', $post->id], 'class' => 'ml-auto']) !!}
+                        {{Form::hidden('_method', 'DELETE')}}
+                        {{Form::token()}}
+                        {{Form::submit('delete', ['class' => 'btn btn-danger'])}}
+                    {!! Form::close() !!}
+                    {{--<form action="" method="POST" class="ml-auto">--}}
+                        {{--{{ method_field('DELETE') }}--}}
+                        {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
+                        {{--<button type="submit" class="btn btn-danger">Delete</button>--}}
+                    {{--</form>--}}
+                    {{--'method' => 'POST'--}}
+                </div>
+            @endforeach
+        @else
+            no posts
+        @endif
+    </ul>
+</div>
+</body>
+</html>
